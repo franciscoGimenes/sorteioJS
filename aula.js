a0544c9c4529be2dcd6790e56cfb2e7ae9a0211f
franciscoGimenes <f.gimenes1316@gmail.com>
2023-08-04 13:26:23 -0300

    Initial commit

---
+++ aula.js
@@ -0,0 +1,7 @@
+console.log(Math.random())
+console.log(Math.random() * 20)
+console.log(Math.random( 7 - 3 + 1) + 3)
+console.log(Math.round(3.1))//arredonda
+console.log(Math.floor())//arredonda pra baixo
+console.log(Math.ceil())//arredonda pra cima
+
